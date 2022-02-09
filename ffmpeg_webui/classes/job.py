58667795cd104e9eddca71bf065addd874f72685
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-02-09 20:27:00 +0100

    add: Enum for status

--- ffmpeg_webui/classes/job.py
+++ ffmpeg_webui/classes/job.py
@@ -1,16 +1,21 @@
 # Definition file for the Job class, representing a single job object
 import json
+from enum import Enum
 from pathlib import Path
 from preset import Preset
 
 
-class TranscodeJob:
+class TranscodeJob(Enum):
+    CREATED = 1
+    RUNNING = 2
+    SUCCESS = 3
+    FAILED = 4
     
     def __init__(self, in_files: list, out_folder: Path, preset: Preset):
         self.in_files = in_files
         self.out_folder = out_folder
         self.preset = preset
-        self.status = "init"
+        self.status = self.CREATED
         self.float_complete = 0.0
 
     def __repr__(self):
