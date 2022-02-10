d3d1b0fec7bdb6c6e52e3a787ab415ce8e201389
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-02-10 10:48:32 +0100

    add: logging config and verbose env var

--- ffmpeg_webui/__init__.py
+++ ffmpeg_webui/__init__.py
@@ -1,3 +1,5 @@
+import logging
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,3 +10,10 @@ api_version = 1
 
 # register API at /api/vX prefix
 app.register_blueprint(rest_api.rest_api, url_prefix=f'/api/v{api_version}')
+
+logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s', level=logging.INFO)
+
+# TODO fix logging config
+if "true" in os.getenv("FFMPEG_WEBUI_VERBOSE").lower():
+    logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s', level=logging.DEBUG)
+    logging.debug("Verbose output enabled")
