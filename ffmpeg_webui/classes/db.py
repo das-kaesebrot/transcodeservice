f1ae2060de7a7e00432e1fb451e33e50b65b166d
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-02-10 23:51:08 +0100

    add: attributes and full init to db handling class

--- ffmpeg_webui/classes/db.py
+++ ffmpeg_webui/classes/db.py
@@ -1,14 +1,48 @@
+import logging, sys
 from pymongo import MongoClient
+from urllib.parse import quote_plus
+from pymongo.errors import ConnectionFailure
 
-# TODO create database handling class
+# This is the database initilization class.
+# 
 
-class DB(MongoClient):
+class DB:
     
-    def __init__(self, host: str = "localhost", port: int = 27017):
-        super().__init__(host = host, port = port)
-        
-    def __test_if_initialized(self):
-        pass
-    
-    def __init(self):
-        pass
+    def __init__(
+            self,
+            username: str,
+            password: str,
+            hostname: str = "localhost",
+            port: int = 27017,
+            database: str = "ffmpeg_webui_db",
+            tz_aware: bool = True,
+            connect: bool = True
+        ):
+
+        self._client = MongoClient(
+                uri = "mongodb://%s:%s@%s:%i" % (
+                    
+                    # In order to be able to connect using a username and password,
+                    # we need to percent encode those paramter to avoid overwriting
+                    # special characters ('@', '/', '+'...) reserved for the path itself
+
+                    quote_plus(username), 
+                    quote_plus(password),
+                    hostname,
+                    port
+                ),
+                tz_aware = tz_aware,
+                connect = connect,
+                appname = "ffmpeg_webui"
+            )
+        self.db = (self._client, database)
+
+        self._try_connection()
+
+
+    def _try_connection(self):
+        try:
+            self._client.admin.command('ping')
+        except ConnectionFailure:
+            logging.error("MongoDB server at %s not available" % self._client.server_info())
+            sys.exit(1)
