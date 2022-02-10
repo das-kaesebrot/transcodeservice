34402c3eacc0b9dc65e6e15883f450c3c67cc8b6
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-02-11 00:20:34 +0100

    add: bootstrap function

--- ffmpeg_webui/__init__.py
+++ ffmpeg_webui/__init__.py
@@ -1,6 +1,9 @@
 import logging
 import os
 from flask import Flask
+from ffmpeg_webui.classes.db import DB
+from ffmpeg_webui.classes.job_service import JobService
+from ffmpeg_webui.classes.config import Config
 
 app = Flask(__name__)
 
@@ -11,9 +14,18 @@ api_version = 1
 # register API at /api/vX prefix
 app.register_blueprint(rest_api.rest_api, url_prefix=f'/api/v{api_version}')
 
-logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s', level=logging.INFO)
+def bootstrap_app():
 
-# TODO fix logging config
-if "true" in os.getenv("FFMPEG_WEBUI_VERBOSE").lower():
-    logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s', level=logging.DEBUG)
-    logging.debug("Verbose output enabled")
+    config = Config()
+
+    logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s', level=logging.INFO)
+
+    # TODO fix logging config
+    if config.verbose:
+        logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s', level=logging.DEBUG)
+        logging.debug("Verbose output enabled")
+
+    # Initialize database connection
+    db = DB(username=config.username, password=config.password, hostname = config.hostname if config.hostname)
+
+bootstrap_app()
