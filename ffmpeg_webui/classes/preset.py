1c4983e5bf8696cb6c6d14d6133084044f639c8e
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-02-11 01:56:04 +0100

    add: first init function for Preset

--- ffmpeg_webui/classes/preset.py
+++ ffmpeg_webui/classes/preset.py
@@ -1,4 +1,12 @@
 # Definition file for the Preset class, representing a single preset object
+# 
+# TODO map options of ffmpeg-python to preset attributes
+# https://kkroening.github.io/ffmpeg-python/
 
-class Preset():
-    pass
+from uuid import UUID
+
+class Preset:
+    
+    def __init__(self, id: UUID, description: str = None):
+        self._id = id
+        self._description = description
