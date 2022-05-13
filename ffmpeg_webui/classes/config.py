e86a8ef16acb652b2d00e09182defd3eeac94a2d
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-05-13 20:47:50 +0200

    add: load and set config settings dynamically

--- ffmpeg_webui/classes/config.py
+++ ffmpeg_webui/classes/config.py
@@ -1,22 +1,25 @@
 import json
+import logging
+import os
+from ffmpeg_webui import app
 from pathlib import Path
 
 class Config:
     FILENAME = "config.json"
     ENV_PREFIX = "FFMPEG_WEBUI"
-    ACCEPTED_VARS = ["verbose", "db_hostname", "db_pass", "db_host"]
-
-    def __init__(self, path: Path = None):
-        self.verbose = False
-        self.username = None
-        self.password = None
-        self.hostname = None
+    ACCEPTED_VARS = ["verbose", "db_hostname", "db_pass", "db_user", "db_port"]
+    
+    def __init__(self, path: str = ""):
         
         app.logger.debug("Entered config setting")
         
         _ = None
+        with open(os.path.join(path, "config.json")) as f:
             _ = json.load(f)
         
-        pass
-
+        for var in self.ACCEPTED_VARS:
+            setattr(self, var, _.get(var))
+            if os.getenv(f"{self.ENV_PREFIX}_{var.upper()}"):
+                setattr(self, var, os.getenv(f"{self.ENV_PREFIX}_{var.upper()}"))
+            
             app.logger.debug(f"Set var \"{var}\": {getattr(self, var)}")
