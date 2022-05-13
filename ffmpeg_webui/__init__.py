06be258c59fc530899125a156cef6d35966b450b
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-05-13 20:47:30 +0200

    fix: prettify __init__ a bit

--- ffmpeg_webui/__init__.py
+++ ffmpeg_webui/__init__.py
@@ -1,6 +1,11 @@
-import logging
 import os
+import logging
 from flask import Flask
+
+# this needs to be created before all other imports
+app = Flask(__name__)
+
+from ffmpeg_webui import routes_ui, rest_api
 from ffmpeg_webui.classes.db import DB
 from ffmpeg_webui.classes.job_service import TranscodejobService
 from ffmpeg_webui.classes.config import Config
@@ -10,7 +15,7 @@ from flask.logging import default_handler
 root = logging.getLogger()
 root.addHandler(default_handler)
 
-from ffmpeg_webui import routes_ui, rest_api
+API_VERSION = 1
 
 dictConfig({
     'version': 1,
@@ -29,10 +34,9 @@ dictConfig({
 })
 
 # register API at /api/vX prefix
-app.register_blueprint(rest_api.rest_api, url_prefix=f'/api/v{api_version}')
+app.register_blueprint(rest_api.rest_api, url_prefix=f'/api/v{API_VERSION}')
 
 def bootstrap_app():
-
     config = Config()
     
     # CHANGE THIS IN PROD
