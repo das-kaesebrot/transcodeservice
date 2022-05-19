e518cd0a5dd097b3ca9c62ef003430432c371d03
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-05-20 01:25:41 +0200

    clean: move initial config into bootstrap function

--- ffmpeg_webui/__init__.py
+++ ffmpeg_webui/__init__.py
@@ -1,65 +1,69 @@
-import os
-import logging
 from flask import Flask
 
 # this needs to be created before all other imports
 app = Flask(__name__)
 
-from ffmpeg_webui import routes_ui, rest_api
+import logging
+from ffmpeg_webui import rest_api, error_handler
 from ffmpeg_webui.classes.config import Config
 from logging.config import dictConfig
 from flask.logging import default_handler
 from pymongo import results
 
-root = logging.getLogger()
-root.addHandler(default_handler)
-
 from json import JSONEncoder
 from uuid import UUID
 
-old_default = JSONEncoder.default
-
-def new_default(self, obj):
-    if isinstance(obj, UUID):
-        return str(obj)
-    elif isinstance(obj, results.InsertOneResult):
-        return {
-            "acknowledged": getattr(obj, "acknowledged"),
-            "inserted_id": getattr(obj, "inserted_id")
-        }
-    return old_default(self, obj)
-
-JSONEncoder.default = new_default
-
 API_VERSION = 1
-
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi']
-    }
-})
 
 # register API at /api/vX prefix
 app.register_blueprint(rest_api.rest_api, url_prefix=f'/api/v{API_VERSION}')
 
-def bootstrap_app():
+def create_app():
+    
+    # configure logging
+    dictConfig({
+        'version': 1,
+        'formatters': {'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }},
+        'handlers': {'wsgi': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://flask.logging.wsgi_errors_stream',
+            'formatter': 'default'
+        }},
+        'root': {
+            'level': 'INFO',
+            'handlers': ['wsgi']
+        }
+    })
+    
+    root = logging.getLogger()
+    root.addHandler(default_handler)
+    
     config = Config()
     
     if config.debug:
         app.logger.setLevel(logging.DEBUG)
         app.logger.debug("Verbose output enabled")
 
-    # Initialize database connection
-    app.logger.info("Startup complete")
+    # fix JSON decoder    
+    old_default = JSONEncoder.default
 
-bootstrap_app()
+    def new_default(self, obj):
+        if isinstance(obj, UUID):
+            return str(obj)
+        elif isinstance(obj, results.InsertOneResult):
+            return {
+                "acknowledged": getattr(obj, "acknowledged"),
+                "inserted_id": getattr(obj, "inserted_id")
+            }
+        return old_default(self, obj)
+
+    JSONEncoder.default = new_default
+
+    app.logger.info("Startup complete")
+    app.config.ERROR_INCLUDE_MESSAGE = False
+
+    return app
+
+create_app()
