3e14280d0a88b337142a3bcbb3f2f610d165062f
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-09-29 21:17:43 +0200

    [FEATURE] adjust presetservice for sqlalchemy syntax

--- transcodeservice/classes/preset_service.py
+++ transcodeservice/classes/preset_service.py
@@ -8,18 +8,27 @@ class PresetService:
         self._session = session
             
     def insert_preset(self, preset: Preset):
-        result = self._collection.insert_one(preset.Simplified())
-        return self.get_preset_by_id(result.inserted_id)
+        result = self._session.add(preset)
+        self._session.commit()
+        self._session.refresh(preset)
+        
+        return preset
+        
+    def update_preset(self, preset: Preset):        
+        self._session.query(Preset).\
+            filter_by(id=preset.id).update(preset.to_dict_for_update())
+        
+        self._session.commit()
+        self._session.refresh(preset)
+        return preset
     
     def get_all_presets(self):
-        return list(self._collection.find())
+        return list(self._session.query(Preset).all())
     
     def get_preset_by_id(self, id):
-        return self._collection.find_one({
-            "_id": id
-        })
+        return self._session.get(Preset, id)
     
     def delete_preset(self, id):
-        return self._collection.delete_one({
-            "_id": id
-        })
+        to_be_deleted = self.get_preset_by_id(id)
+        self._session.delete(to_be_deleted)
+        self._session.commit()
