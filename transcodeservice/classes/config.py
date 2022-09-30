894dacfa165ac0a579b4adf4c033372f13a519f4
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-09-30 16:54:25 +0200

    [FEATURE] refactor config for better parsing and fallback vars

--- transcodeservice/classes/config.py
+++ transcodeservice/classes/config.py
@@ -1,14 +1,24 @@
 import json
 import os
 
-from attr import has
 from transcodeservice import app
-from pathlib import Path
+
+from transcodeservice.util.str2bool import str2bool
 
 class Config:
     FILENAME = "config.json"
     ENV_PREFIX = "TRANSCODESERVICE"
-    ACCEPTED_VARS = ["debug", "db_string", "db_debug_mode", "db_hostname", "db_pass", "db_user", "db_port"]
+    ACCEPTED_VARS_WITH_DEFAULTS = [
+        ("debug", bool, False),
+        ("db_string", str, None),
+        ("db_debug_mode", bool, False),
+        ("db_host", str, None),
+        ("db_pass", str, None),
+        ("db_user", str, None),
+        ("db_port", int, 0),
+        ("db_database", str, "transcodeservice.db"),
+        ("db_dialect", str, "sqlite")
+    ]
     
     def __init__(self, path: str = ""):
         
@@ -19,16 +29,35 @@ class Config:
             with open(os.path.join(path, self.FILENAME)) as f:
                 _ = json.load(f)
         
-        for var in self.ACCEPTED_VARS:
-            if _:
+        for var_tuple in self.ACCEPTED_VARS_WITH_DEFAULTS:
+            var = var_tuple[0]
+            expected_type = var_tuple[1]
+            defaultval = var_tuple[2]
+            env_var = f"{self.ENV_PREFIX}_{var.upper()}"
+            
+            if _.get(var):
                 setattr(self, var, _.get(var))
+                app.logger.debug(f"Set var \"{var}\" ({expected_type.__name__}) from {self.FILENAME}: {getattr(self, var)}")
             
-            if os.getenv(f"{self.ENV_PREFIX}_{var.upper()}"):
-                setattr(self, var, os.getenv(f"{self.ENV_PREFIX}_{var.upper()}"))
-            
-            if hasattr(self, var):
-                app.logger.debug(f"Set var \"{var}\": {getattr(self, var)}")
+            # sometimes wishing that I used a language with strict typing
+            if os.getenv(env_var):
+                value = os.getenv(f"{self.ENV_PREFIX}_{var.upper()}")
+                if expected_type == bool:
+                    value = str2bool(value)
                 
+                setattr(self, var, value)
+                
+                app.logger.debug(f"Set var \"{var}\" ({expected_type.__name__}) from env var {env_var}: {value}")
+                        
+            if not hasattr(self, var):
+                setattr(self, var, defaultval)
+                app.logger.debug(f"Set var \"{var}\" ({expected_type.__name__}) to fallback: {defaultval}")
+        
+        for var_tuple in self.ACCEPTED_VARS_WITH_DEFAULTS:
+            var = var_tuple[0]
+            expected_type = var_tuple[1]
+            app.logger.debug(f"Config var \"{var}\" ({expected_type.__name__}): {getattr(self, var)}")
+        
         if not self.db_string:
             self.db_string = "sqlite:///:memory:"
             
