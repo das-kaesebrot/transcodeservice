77f2ebb818c7b414f498fd0e6d62aaa8db14b325
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-10-13 11:11:10 +0200

    [RENAME] prepend "I" for job repo interface

--- src/main/java/eu/kaesebrot/transcodeservice/services/ITranscodeJobRepository.java
+++ src/main/java/eu/kaesebrot/transcodeservice/services/ITranscodeJobRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface TranscodeJobRepository extends JpaRepository<TranscodeJob, UUID> {
+public interface ITranscodeJobRepository extends JpaRepository<TranscodeJob, UUID> {
 
 }
