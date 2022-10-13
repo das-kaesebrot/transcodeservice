36bea120f480a8e13007d06a3672d77a6003b10f
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-10-14 00:11:39 +0200

    [API] route for getting all jobs

--- src/main/java/eu/kaesebrot/transcodeservice/api/TranscodeServiceRestController.java
+++ src/main/java/eu/kaesebrot/transcodeservice/api/TranscodeServiceRestController.java
@@ -1,5 +1,10 @@
 package eu.kaesebrot.transcodeservice.api;
 
+import eu.kaesebrot.transcodeservice.models.TranscodeJob;
+import eu.kaesebrot.transcodeservice.services.ITranscodeJobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class TranscodeServiceRestController {
+    @Autowired
+    ITranscodeJobService jobService;
 
     @GetMapping(value = "ping")
     public String Ping() {
         return "pong";
     }
+
+    @GetMapping(value = "jobs")
+    public Page<TranscodeJob> GetAllJobs(Pageable pageable) {
+        return jobService.GetAllJobsPaged(pageable);
+    }
+
 }
