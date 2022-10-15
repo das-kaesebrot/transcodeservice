f57b878bddcf5e060b1ad4ec079b147c8d2b6f48
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-10-15 21:30:48 +0200

    [FEATURE] routes for getting a single job, creating new and patching

--- src/main/java/eu/kaesebrot/transcodeservice/api/TranscodeServiceRestController.java
+++ src/main/java/eu/kaesebrot/transcodeservice/api/TranscodeServiceRestController.java
@@ -1,16 +1,17 @@
 package eu.kaesebrot.transcodeservice.api;
 
 import eu.kaesebrot.transcodeservice.models.TranscodeJob;
+import eu.kaesebrot.transcodeservice.models.TranscodeJobUpdate;
 import eu.kaesebrot.transcodeservice.services.ITranscodeJobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/transcodeservice")
 public class TranscodeServiceRestController {
     @Autowired
     ITranscodeJobService jobService;
@@ -25,4 +26,18 @@ public class TranscodeServiceRestController {
         return jobService.GetAllJobsPaged(pageable);
     }
 
+    @PostMapping(value = "jobs")
+    public TranscodeJob CreateNewJob(TranscodeJob job) {
+        return jobService.InsertJob(job);
+    }
+
+    @GetMapping(value = "jos/{id}")
+    public TranscodeJob GetJob(@PathVariable UUID id) {
+        return jobService.GetJob(id);
+    }
+
+    @PatchMapping(value = "jobs/{id}")
+    public TranscodeJob UpdateJob(@PathVariable UUID id, TranscodeJobUpdate transcodeJobUpdate) {
+        return jobService.UpdateJob(transcodeJobUpdate, id);
+    }
 }
