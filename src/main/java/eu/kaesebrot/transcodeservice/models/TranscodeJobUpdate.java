0978ce332d53fe426fb87d4df438f1fa35b23191
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-10-23 19:40:02 +0200

    [FEATURE] add Preset field in TranscodeJobUpdate

--- src/main/java/eu/kaesebrot/transcodeservice/models/TranscodeJobUpdate.java
+++ src/main/java/eu/kaesebrot/transcodeservice/models/TranscodeJobUpdate.java
@@ -1,16 +1,20 @@
 package eu.kaesebrot.transcodeservice.models;
 
+import eu.kaesebrot.transcodeservice.services.ITranscodePresetService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.io.Serializable;
 import java.util.Optional;
 import java.util.UUID;
 
 public class TranscodeJobUpdate implements Serializable {
+    @Autowired
+    private ITranscodePresetService presetService;
 
     private Optional<String> InFile;
 
     private Optional<String> OutFolder;
-
-    private Optional<UUID> PresetId;
+    private Optional<TranscodePreset> Preset;
 
     public Optional<String> getInFile() {
         return InFile;
@@ -27,17 +31,23 @@ public class TranscodeJobUpdate implements Serializable {
         OutFolder = outFolder;
     }
 
-    public Optional<UUID> getPresetId() {
-        return PresetId;
+    public Optional<TranscodePreset> getPreset() {
+        return Preset;
     }
 
-    public void setPresetId(Optional<UUID> presetId) {
-        PresetId = presetId;
+    public void setPreset(Optional<UUID> presetId) {
+        if (presetId.isPresent()) {
+            Preset = presetService.GetPresetOptional(presetId.get());
+        }
     }
 
     public TranscodeJobUpdate(Optional<String> inFile, Optional<String> outFolder, Optional<UUID> presetId) {
         InFile = inFile;
         OutFolder = outFolder;
-        PresetId = presetId;
+        if (presetId.isPresent()) {
+            Preset = presetService.GetPresetOptional(presetId.get());
+        } else {
+            Preset = Optional.empty();
+        }
     }
 }
