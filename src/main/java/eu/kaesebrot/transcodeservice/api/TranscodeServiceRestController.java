cce21c6607c6a262506659f1a70aeb8a930c0b06
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-10-28 19:27:00 +0200

    [FEATURE] route for creating a new preset

--- src/main/java/eu/kaesebrot/transcodeservice/api/TranscodeServiceRestController.java
+++ src/main/java/eu/kaesebrot/transcodeservice/api/TranscodeServiceRestController.java
@@ -1,8 +1,11 @@
 package eu.kaesebrot.transcodeservice.api;
 
 import eu.kaesebrot.transcodeservice.models.TranscodeJob;
+import eu.kaesebrot.transcodeservice.models.TranscodeJobNew;
 import eu.kaesebrot.transcodeservice.models.TranscodeJobUpdate;
+import eu.kaesebrot.transcodeservice.models.TranscodePreset;
 import eu.kaesebrot.transcodeservice.services.ITranscodeJobService;
+import eu.kaesebrot.transcodeservice.services.ITranscodePresetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +18,9 @@ import java.util.UUID;
 public class TranscodeServiceRestController {
     @Autowired
     ITranscodeJobService jobService;
+
+    @Autowired
+    ITranscodePresetService presetService;
 
     @GetMapping(value = "ping")
     public String Ping() {
@@ -41,5 +47,10 @@ public class TranscodeServiceRestController {
     @PatchMapping(value = "jobs/{id}")
     public TranscodeJob UpdateJob(@PathVariable UUID id, TranscodeJobUpdate transcodeJobUpdate) {
         return jobService.UpdateJob(transcodeJobUpdate, id);
+    }
+
+    @PostMapping(value = "presets")
+    public TranscodePreset CreateNewPreset() {
+        return presetService.InsertPreset(new TranscodePreset());
     }
 }
