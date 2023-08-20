f806bd845949453856139acf4256cd16a1206e1a
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-08-20 11:02:24 +0200

    [FEATURE] remove unused functions in transcode job service, use custom repo methods

--- src/main/java/eu/kaesebrot/dev/transcodeservice/services/ITranscodeJobService.java
+++ src/main/java/eu/kaesebrot/dev/transcodeservice/services/ITranscodeJobService.java
@@ -7,30 +7,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.Dictionary;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 public interface ITranscodeJobService
 {
     public Optional<TranscodeJob> GetJobOptional(Long id);
     public TranscodeJob GetJob(Long id) throws EntityNotFoundException;
     public Page<TranscodeJob> GetAllJobsPaged(Pageable pageable);
-    public List<TranscodeJob> GetRunningJobs();
-    public List<TranscodeJob> GetJobsByStatusList(List<ETranscodeServiceStatus> statusList) throws EntityNotFoundException;
-    public Stream<TranscodeJob> GetJobStreamByStatusList(List<ETranscodeServiceStatus> statusList) throws EntityNotFoundException;
-    public List<TranscodeJob> GetJobsUsingPresetId(Long presetId);
     public TranscodeJob InsertJob(TranscodeJob transcodeJob);
     public TranscodeJob UpdateJob(TranscodeJob transcodeJob);
-    public TranscodeJob UpdateJob(Dictionary<String, Object> updateData, Long jobId);
     public TranscodeJob UpdateJob(TranscodeJobUpdate updateData, Long jobId);
-    public long CountAllJobs();
-    public long CountSuccessfulJobs();
-    public long CountFailedJobs();
-    public long CountJobsWithStatusList(List<ETranscodeServiceStatus> statusList);
-    public void DeleteJob(TranscodeJob transcodeJob);
     public void DeleteJobById(Long id);
-    public void DeleteAllFailedJobs();
     public void DeleteByStatusList(List<ETranscodeServiceStatus> statusList);
 }
--- src/main/java/eu/kaesebrot/dev/transcodeservice/services/TranscodeJobService.java
+++ src/main/java/eu/kaesebrot/dev/transcodeservice/services/TranscodeJobService.java
@@ -3,18 +3,15 @@ package eu.kaesebrot.dev.transcodeservice.services;
 import eu.kaesebrot.dev.transcodeservice.constants.ETranscodeServiceStatus;
 import eu.kaesebrot.dev.transcodeservice.models.TranscodeJob;
 import eu.kaesebrot.dev.transcodeservice.models.rest.TranscodeJobUpdate;
-import org.hibernate.cfg.NotYetImplementedException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.Dictionary;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
-import java.util.stream.Stream;
 
 @Service
 public class TranscodeJobService implements ITranscodeJobService
@@ -53,11 +50,6 @@ public class TranscodeJobService implements ITranscodeJobService
     }
 
     @Override
-    public TranscodeJob UpdateJob(Dictionary<String, Object> updateData, Long jobId) {
-        throw new NotYetImplementedException();
-    }
-
-    @Override
     public TranscodeJob UpdateJob(TranscodeJobUpdate updateData, Long jobId) {
         var job = GetJob(jobId);
 
@@ -72,28 +64,6 @@ public class TranscodeJobService implements ITranscodeJobService
         }
 
         return UpdateJob(job);
-    }
-
-    @Override
-    public long CountAllJobs() {
-        return repository.count();
-    }
-
-    @Override
-    public long CountSuccessfulJobs() {
-        return CountJobsWithStatusList(List.of(ETranscodeServiceStatus.SUCCESS));
-    }
-
-    @Override
-    public long CountFailedJobs() {
-        return CountJobsWithStatusList(List.of(ETranscodeServiceStatus.FAILED));
-    }
-
-    @Override
-    public long CountJobsWithStatusList(List<ETranscodeServiceStatus> statusList) {
-        return
-                GetJobStreamByStatusList(statusList)
-                        .count();
     }
 
     @Override
@@ -114,83 +84,12 @@ public class TranscodeJobService implements ITranscodeJobService
     }
 
     @Override
-    public List<TranscodeJob> GetRunningJobs() {
-        return GetJobsByStatusList(List.of(ETranscodeServiceStatus.RUNNING));
-    }
-
-    @Override
-    public void DeleteJob(TranscodeJob transcodeJob) {
-        if (transcodeJob != null) {
-            DeleteJobById(transcodeJob.getId());
-        }
-    }
-
-    @Override
     public void DeleteJobById(Long id) {
-        try {
-            jobLock.writeLock().lock();
-
-            repository.deleteById(id);
-        } finally {
-            jobLock.writeLock().unlock();
-        }
-    }
-
-    @Override
-    public void DeleteAllFailedJobs() {
-        DeleteByStatusList(List.of(ETranscodeServiceStatus.FAILED));
+        repository.deleteById(id);
     }
 
     @Override
     public void DeleteByStatusList(List<ETranscodeServiceStatus> statusList) {
-        var result = GetJobsByStatusList(statusList);
-
-        try {
-            jobLock.writeLock().lock();
-            repository.deleteAll(result);
-        } finally {
-            jobLock.writeLock().unlock();
-        }
-    }
-
-    @Override
-    public List<TranscodeJob> GetJobsByStatusList(List<ETranscodeServiceStatus> statusList) throws EntityNotFoundException {
-        var result = repository
-                .findAll()
-                .stream()
-                .filter(t -> statusList.contains(t.getTranscodeStatus().getStatusEnum()))
-                .sorted()
-                .toList();
-
-        if (result.isEmpty()) {
-            throw new EntityNotFoundException(String.format("Can't find any jobs with status: %s", statusList));
-        }
-
-        return result;
-    }
-
-    @Override
-    public Stream<TranscodeJob> GetJobStreamByStatusList(List<ETranscodeServiceStatus> statusList) throws EntityNotFoundException {
-        var result = repository
-                .findAll()
-                .stream()
-                .filter(t -> statusList.contains(t.getTranscodeStatus().getStatusEnum()))
-                .sorted();
-
-        if (result.findAny().isEmpty()) {
-            throw new EntityNotFoundException(String.format("Can't find any jobs with status: %s", statusList));
-        }
-
-        return result;
-    }
-
-    @Override
-    public List<TranscodeJob> GetJobsUsingPresetId(Long presetId) {
-        return repository
-                .findAll()
-                .stream()
-                .filter(t -> t.getPreset().getId() == presetId)
-                .sorted()
-                .toList();
+        repository.deleteAllByTranscodeStatusIn(statusList);
     }
 }
