e97dc396fdd57041c10463d5f1b3562f54110744
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-08-21 19:00:59 +0200

    [FEATURE] add utility methods for getting supported video and audio codecs and muxers

--- src/main/java/eu/kaesebrot/dev/transcodeservice/ffmpeg/FfmpegUtilities.java
+++ src/main/java/eu/kaesebrot/dev/transcodeservice/ffmpeg/FfmpegUtilities.java
@@ -1,9 +1,15 @@
 package eu.kaesebrot.dev.transcodeservice.ffmpeg;
 
-import java.io.IOException;
+import com.github.manevolent.ffmpeg4j.FFmpegException;
+import org.bytedeco.ffmpeg.avformat.AVOutputFormat;
+
 import java.util.List;
 
 public interface FfmpegUtilities {
-    List<String> getSupportedVideoCodecs() throws IOException;
-    List<String> getSupportedAudioCodecs();
+    List<String> getSupportedVideoEncoders();
+    List<String> getSupportedAudioEncoders();
+    List<String> getSupportedVideoDecoders();
+    List<String> getSupportedAudioDecoders();
+    List<AVOutputFormat> getSupportedMuxers();
+    boolean muxerSupportsCodec(AVOutputFormat muxer, String codecName) throws FFmpegException;
 }
