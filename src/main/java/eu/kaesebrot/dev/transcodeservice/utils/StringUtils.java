20575e76e4419c129ac135132f3b7ecdf1f3c0e0
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-08-22 23:57:54 +0200

    [FEATURE] add StringUtils

---
+++ src/main/java/eu/kaesebrot/dev/transcodeservice/utils/StringUtils.java
@@ -0,0 +1,19 @@
+package eu.kaesebrot.dev.transcodeservice.utils;
+
+public final class StringUtils {
+    private StringUtils() {}
+
+    public static boolean isNullOrEmpty(String string) {
+        return (string == null || string.trim().isEmpty() || string.trim().isBlank());
+    }
+
+    public static String getFilenameWithoutExtension(String filename) {
+        int pos = filename.lastIndexOf(".");
+
+        if (pos > 0 && pos < (filename.length() - 1)) {
+            return filename.substring(0, pos);
+        }
+
+        return filename;
+    }
+}
