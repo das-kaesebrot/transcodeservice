9b06b7d097f340a0b6ebdb47149f5987d0c0b18a
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-08-22 02:52:59 +0200

    [FEATURE] implement audio substream processing

--- src/main/java/eu/kaesebrot/dev/transcodeservice/ffmpeg/FFmpegRunnable.java
+++ src/main/java/eu/kaesebrot/dev/transcodeservice/ffmpeg/FFmpegRunnable.java
@@ -1,11 +1,13 @@
 package eu.kaesebrot.dev.transcodeservice.ffmpeg;
 
 import com.github.manevolent.ffmpeg4j.*;
+import com.github.manevolent.ffmpeg4j.source.AudioSourceSubstream;
 import com.github.manevolent.ffmpeg4j.source.VideoSourceSubstream;
 import com.github.manevolent.ffmpeg4j.stream.output.FFmpegTargetStream;
 import com.github.manevolent.ffmpeg4j.stream.source.FFmpegSourceStream;
 import com.github.manevolent.ffmpeg4j.transcoder.Transcoder;
 import eu.kaesebrot.dev.transcodeservice.constants.ETrackPresetType;
+import eu.kaesebrot.dev.transcodeservice.models.AudioTrackPreset;
 import eu.kaesebrot.dev.transcodeservice.models.TranscodeJob;
 import eu.kaesebrot.dev.transcodeservice.models.TranscodePreset;
 import eu.kaesebrot.dev.transcodeservice.models.VideoTrackPreset;
@@ -44,6 +46,11 @@ public class FFmpegRunnable implements Runnable {
                     .filter(t -> t.getType().equals(ETrackPresetType.VIDEO))
                     .findFirst().orElseThrow();
 
+            AudioTrackPreset audioPreset = (AudioTrackPreset) preset
+                    .getTrackPresets().stream()
+                    .filter(t -> t.getType().equals(ETrackPresetType.AUDIO))
+                    .findFirst().orElseThrow();
+
             VideoFormat inputFormat = videoStream.getFormat();
 
             int width = inputFormat.getWidth();
@@ -64,7 +71,22 @@ public class FFmpegRunnable implements Runnable {
             target.registerVideoSubstream(videoPreset.getVideoCodecName(), width, height, frame_rate, new HashMap<>());
             // target.setPixelFormat(avutil.AV_PIX_FMT_RGB24);
 
-            // TODO implement processing for audio tracks
+            for (AudioSourceSubstream audioSubstream : source.getSubstreams().stream()
+                    .filter(s -> s.getMediaType() == MediaType.AUDIO)
+                    .map(AudioSourceSubstream.class::cast)
+                    .toList()) {
+
+                var inputAudioFormat = audioSubstream.getFormat();
+
+                int samplerate = inputAudioFormat.getSampleRate();
+
+                if (audioPreset.getAudioSampleRate() != null && audioPreset.getAudioSampleRate() != samplerate) {
+                    samplerate = audioPreset.getAudioSampleRate();
+                }
+
+                target.registerAudioSubstream(audioPreset.getAudioCodecName(), samplerate, inputAudioFormat.getChannels(), inputAudioFormat.getChannelLayout(), new HashMap<>());
+            }
+
             sourceStream = source;
             targetStream = target;
 
