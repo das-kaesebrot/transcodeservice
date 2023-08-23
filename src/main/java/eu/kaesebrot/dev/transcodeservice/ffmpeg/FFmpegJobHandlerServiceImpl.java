03d8f3423e0a43246ca74fc5e64f97315c6fe953
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-08-24 00:11:36 +0200

    [FEATURE] return list of callables from handler service

--- src/main/java/eu/kaesebrot/dev/transcodeservice/ffmpeg/FFmpegJobHandlerServiceImpl.java
+++ src/main/java/eu/kaesebrot/dev/transcodeservice/ffmpeg/FFmpegJobHandlerServiceImpl.java
@@ -3,30 +3,29 @@ package eu.kaesebrot.dev.transcodeservice.ffmpeg;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.*;
-import java.util.stream.Stream;
 
 @Service
 public class FFmpegJobHandlerServiceImpl implements FFmpegJobHandlerService {
-
     private final ExecutorService executor = Executors.newFixedThreadPool(10);
     private final Map<Callable<?>, Future> submittedTasks = new HashMap<>();
-    private Runnable active = null;
-
     public FFmpegJobHandlerServiceImpl() {
 
     }
 
     @Override
     public void submit(Callable<?> task) {
-        submittedTasks.put(task, executor.submit(task));
+        var future = executor.submit(task);
+        submittedTasks.put(task, future);
     }
 
     @Override
-    public Stream<Callable<?>> getCompletedTasks() {
+    public List<? extends Callable<?>> getCompletedTasks() {
         return submittedTasks.entrySet().stream()
                 .filter(e -> e.getValue().isDone())
-                .map(e -> e.getKey());
+                .map(Map.Entry::getKey)
+                .toList();
     }
 }
