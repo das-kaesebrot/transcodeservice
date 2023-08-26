a7206a87989d5f3ab4703d3b1a3711058e46c856
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-08-26 19:53:16 +0200

    [FEATURE] remove unused codec enums

--- src/main/java/eu/kaesebrot/dev/transcodeservice/constants/AudioCodecs.java
+++
@@ -1,6 +0,0 @@
-package eu.kaesebrot.dev.transcodeservice.constants;
-
-public enum AudioCodecs {
-    AAC,
-    PCM,
-}
--- src/main/java/eu/kaesebrot/dev/transcodeservice/constants/VideoCodecs.java
+++
@@ -1,6 +0,0 @@
-package eu.kaesebrot.dev.transcodeservice.constants;
-
-public enum VideoCodecs {
-    H264,
-    H265,
-}
