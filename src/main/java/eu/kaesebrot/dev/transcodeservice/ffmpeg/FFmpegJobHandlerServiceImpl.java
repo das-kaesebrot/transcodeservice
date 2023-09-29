7a3446e201131f863a94e72c23ff0f570867b951
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-09-29 15:22:33 +0200

    [FIX] move generation of transcoder and validation completely to callable

--- src/main/java/eu/kaesebrot/dev/transcodeservice/ffmpeg/FFmpegJobHandlerServiceImpl.java
+++ src/main/java/eu/kaesebrot/dev/transcodeservice/ffmpeg/FFmpegJobHandlerServiceImpl.java
@@ -1,47 +1,41 @@
 package eu.kaesebrot.dev.transcodeservice.ffmpeg;
 
-import com.github.manevolent.ffmpeg4j.FFmpeg;
-import com.github.manevolent.ffmpeg4j.FFmpegIO;
-import com.github.manevolent.ffmpeg4j.MediaType;
-import com.github.manevolent.ffmpeg4j.VideoFormat;
-import com.github.manevolent.ffmpeg4j.filter.audio.AudioFilterNone;
-import com.github.manevolent.ffmpeg4j.filter.video.VideoFilterNone;
-import com.github.manevolent.ffmpeg4j.source.AudioSourceSubstream;
-import com.github.manevolent.ffmpeg4j.source.VideoSourceSubstream;
-import com.github.manevolent.ffmpeg4j.stream.output.FFmpegTargetStream;
-import com.github.manevolent.ffmpeg4j.stream.source.FFmpegSourceStream;
 import com.github.manevolent.ffmpeg4j.transcoder.Transcoder;
-import eu.kaesebrot.dev.transcodeservice.constants.ETrackPresetType;
-import eu.kaesebrot.dev.transcodeservice.models.AudioTrackPreset;
+import eu.kaesebrot.dev.transcodeservice.constants.ETranscodeServiceStatus;
 import eu.kaesebrot.dev.transcodeservice.models.TranscodeJob;
-import eu.kaesebrot.dev.transcodeservice.models.TranscodePreset;
-import eu.kaesebrot.dev.transcodeservice.models.VideoTrackPreset;
-import eu.kaesebrot.dev.transcodeservice.services.ITranscodeJobRepository;
-import eu.kaesebrot.dev.transcodeservice.utils.AVUtils;
-import eu.kaesebrot.dev.transcodeservice.utils.StringUtils;
+import eu.kaesebrot.dev.transcodeservice.services.ITranscodePresetRepository;
+import eu.kaesebrot.dev.transcodeservice.services.TranscodeJobService;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.util.*;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Service
-public class FFmpegJobHandlerServiceImpl implements FFmpegJobHandlerService {
-    private final ExecutorService executor = Executors.newWorkStealingPool();
+public class FFmpegJobHandlerServiceImpl implements JobHandlerService {
+    private final ExecutorService executor = Executors.newFixedThreadPool(10);
     private final Map<Long, Future<?>> submittedTasks = new HashMap<>();
     private final Map<Long, Transcoder> transcoders = new HashMap<>();
-    private final ITranscodeJobRepository transcodeJobRepository;
-    public FFmpegJobHandlerServiceImpl(ITranscodeJobRepository transcodeJobRepository) {
-        this.transcodeJobRepository = transcodeJobRepository;
+    private final ITranscodePresetRepository presetRepository;
+    private final TranscodeJobService jobService;
+    public FFmpegJobHandlerServiceImpl(ITranscodePresetRepository presetRepository, TranscodeJobService jobService) {
+        this.presetRepository = presetRepository;
+        this.jobService = jobService;
+    }
+
+    @Override
+    public void submit(Long jobId) {
+
     }
 
     @Override
     public void submit(TranscodeJob job) {
-        Transcoder transcoder = getTranscoderForJob(job);
-        FFmpegTranscodingCallable transcodingCallable = new FFmpegTranscodingCallable(transcoder);
+        FFmpegTranscodingCallable transcodingCallable = new FFmpegTranscodingCallable(job, job.getPreset(), job.getPreset().getTrackPresets());
 
         Future<?> transcodingFuture = executor.submit(transcodingCallable);
         submittedTasks.put(job.getId(), transcodingFuture);
+        jobService.setJobStatus(job.getId(), ETranscodeServiceStatus.QUEUED);
     }
 
     @Override
@@ -50,10 +44,15 @@ public class FFmpegJobHandlerServiceImpl implements FFmpegJobHandlerService {
 
         for (var taskKeyValuePair : submittedTasks.entrySet()) {
             if (taskKeyValuePair.getValue().isDone())
-                resultList.add(transcodeJobRepository.findById(taskKeyValuePair.getKey()).orElseThrow());
+                resultList.add(jobService.getJob(taskKeyValuePair.getKey()));
         }
 
         return resultList;
+    }
+
+    @Override
+    public double getProgress(Long jobId) throws NoSuchElementException {
+        return 0;
     }
 
     @Override
@@ -66,94 +65,5 @@ public class FFmpegJobHandlerServiceImpl implements FFmpegJobHandlerService {
                 .getProgress();
          */
         return -1D; // TODO implement progess getter
-    }
-
-
-    /**
-     * Either retrieves an existing {@link com.github.manevolent.ffmpeg4j.transcoder.Transcoder Transcoder} object for the specified job
-     * or generates one on the fly, adds it to the list of known jobs and returns it.
-     * @param job The job to get or generate a {@link com.github.manevolent.ffmpeg4j.transcoder.Transcoder Transcoder} for
-     * @return A new or already present {@link com.github.manevolent.ffmpeg4j.transcoder.Transcoder Transcoder}.
-     */
-    private Transcoder getTranscoderForJob(TranscodeJob job) {
-        return getTranscoderForJob(job, false);
-    }
-
-    private Transcoder getTranscoderForJob(TranscodeJob job, boolean forceRecreation) {
-        if (transcoders.containsKey(job.getId()) && !forceRecreation)
-            return transcoders.get(job.getId());
-
-        File inFile = new File(job.getInFile());
-        File outFile = job.getOutFileName().toFile();
-
-        TranscodePreset preset = job.getPreset();
-
-        if (!inFile.isFile())
-            throw new IllegalArgumentException("File does not exist at given path! Path: " + job.getInFile());
-
-        try (FFmpegSourceStream source = FFmpegIO.openInput(inFile).open(AVUtils.getInputFormat(inFile.getAbsolutePath()));
-             FFmpegTargetStream target = FFmpegIO.openOutput(outFile).open(preset.getMuxer())) {
-            source.registerStreams();
-
-            var videoStream = (VideoSourceSubstream) source.getSubstreams().stream().filter(s -> s.getMediaType() == MediaType.VIDEO).findFirst().orElseThrow();
-
-            VideoTrackPreset videoPreset = (VideoTrackPreset) preset
-                    .getTrackPresets().stream()
-                    .filter(t -> t.getType().equals(ETrackPresetType.VIDEO))
-                    .findFirst().orElseThrow();
-
-            AudioTrackPreset audioPreset = (AudioTrackPreset) preset
-                    .getTrackPresets().stream()
-                    .filter(t -> t.getType().equals(ETrackPresetType.AUDIO))
-                    .findFirst().orElseThrow();
-
-            VideoFormat inputFormat = videoStream.getFormat();
-
-            int width = inputFormat.getWidth();
-            int height = inputFormat.getHeight();
-            double frame_rate = inputFormat.getFramesPerSecond();
-
-            if (videoPreset.getWidth() != null && videoPreset.getWidth() != width) {
-                width = videoPreset.getWidth();
-            }
-            if (videoPreset.getHeight() != null && videoPreset.getHeight() != height) {
-                height = videoPreset.getHeight();
-            }
-            if (videoPreset.getFramerate() != null && (1D / videoPreset.getFramerate()) != frame_rate) {
-                frame_rate = 1D / videoPreset.getFramerate();
-            }
-
-            // TODO implement gamut, target color space
-            var videoOpts = new HashMap<String, String>();
-
-            target.registerVideoSubstream(videoPreset.getVideoCodecName(), width, height, frame_rate, videoOpts);
-
-            if (!StringUtils.isNullOrEmpty(videoPreset.getVideoPixelFormat()))
-                target.setPixelFormat(FFmpeg.getPixelFormatByName(videoPreset.getVideoPixelFormat()));
-
-            for (AudioSourceSubstream audioSubstream : source.getSubstreams().stream()
-                    .filter(s -> s.getMediaType() == MediaType.AUDIO)
-                    .map(AudioSourceSubstream.class::cast)
-                    .toList()) {
-
-                var inputAudioFormat = audioSubstream.getFormat();
-
-                int samplerate = inputAudioFormat.getSampleRate();
-
-                if (audioPreset.getAudioSampleRate() != null && audioPreset.getAudioSampleRate() != samplerate) {
-                    samplerate = audioPreset.getAudioSampleRate();
-                }
-
-                target.registerAudioSubstream(audioPreset.getAudioCodecName(), samplerate, inputAudioFormat.getChannels(), inputAudioFormat.getChannelLayout(), new HashMap<>());
-            }
-
-            Transcoder transcoder = new Transcoder(source, target, new AudioFilterNone(), new VideoFilterNone(), Double.MAX_VALUE);
-
-            transcoders.put(job.getId(), transcoder);
-            return transcoder;
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 }
--- src/main/java/eu/kaesebrot/dev/transcodeservice/ffmpeg/FFmpegTranscodingCallable.java
+++ src/main/java/eu/kaesebrot/dev/transcodeservice/ffmpeg/FFmpegTranscodingCallable.java
@@ -1,19 +1,169 @@
 package eu.kaesebrot.dev.transcodeservice.ffmpeg;
 
+import com.github.manevolent.ffmpeg4j.FFmpeg;
+import com.github.manevolent.ffmpeg4j.FFmpegIO;
+import com.github.manevolent.ffmpeg4j.MediaType;
+import com.github.manevolent.ffmpeg4j.VideoFormat;
+import com.github.manevolent.ffmpeg4j.filter.audio.AudioFilterNone;
+import com.github.manevolent.ffmpeg4j.filter.video.VideoFilterNone;
+import com.github.manevolent.ffmpeg4j.source.AudioSourceSubstream;
+import com.github.manevolent.ffmpeg4j.source.VideoSourceSubstream;
+import com.github.manevolent.ffmpeg4j.stream.output.FFmpegTargetStream;
+import com.github.manevolent.ffmpeg4j.stream.source.FFmpegSourceStream;
 import com.github.manevolent.ffmpeg4j.transcoder.Transcoder;
+import eu.kaesebrot.dev.transcodeservice.constants.ETrackPresetType;
+import eu.kaesebrot.dev.transcodeservice.models.*;
+import eu.kaesebrot.dev.transcodeservice.utils.AVUtils;
+import eu.kaesebrot.dev.transcodeservice.utils.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.io.File;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.HashMap;
+import java.util.Set;
 import java.util.concurrent.Callable;
 
+@Transactional
 public class FFmpegTranscodingCallable implements Callable<Void> {
-    private final Transcoder transcoder;
+    private final TranscodeJob job;
+    private final TranscodePreset preset;
+    private final Set<TrackPreset> trackPresetSet;
+    private final Logger logger;
 
-    public FFmpegTranscodingCallable(Transcoder transcoder) {
-        this.transcoder = transcoder;
+    public FFmpegTranscodingCallable(TranscodeJob job, TranscodePreset preset, Set<TrackPreset> trackPresetSet) {
+        this.job = job;
+        this.preset = preset;
+        this.trackPresetSet = trackPresetSet;
+
+        this.trackPresetSet.size();
+        this.logger = LoggerFactory.getLogger("transcoder-job-" + job.getId());
     }
 
     @Override
     public Void call() throws Exception {
-        transcoder.transcode();
+        logger.info("Generating transcoder options");
+
+        File inFile = new File(job.getInFile());
+        Path outFilePath = job.getOutFileName();
+
+        if (!inFile.isFile())
+            throw new IllegalArgumentException("File does not exist at given path! Path: " + job.getInFile());
+
+        SeekableByteChannel outChannel = Files.newByteChannel(outFilePath, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+        FFmpegTargetStream target = FFmpegIO.openChannel(outChannel).asOutput().open(preset.getMuxer());
+
+        try (FFmpegSourceStream source = FFmpegIO.openInput(inFile).open(AVUtils.getInputFormat(inFile.getAbsolutePath()))) {
+            source.registerStreams();
+
+            var videoStream = (VideoSourceSubstream) source.getSubstreams().stream().filter(s -> s.getMediaType() == MediaType.VIDEO).findFirst().orElseThrow();
+
+            VideoTrackPreset videoPreset = (VideoTrackPreset) trackPresetSet.stream()
+                    .filter(t -> t.getType().equals(ETrackPresetType.VIDEO))
+                    .findFirst().orElseThrow();
+
+            AudioTrackPreset audioPreset = (AudioTrackPreset) trackPresetSet.stream()
+                    .filter(t -> t.getType().equals(ETrackPresetType.AUDIO))
+                    .findFirst().orElseThrow();
+
+            VideoFormat inputFormat = videoStream.getFormat();
+
+            int width = inputFormat.getWidth();
+            int height = inputFormat.getHeight();
+            double frame_rate = inputFormat.getFramesPerSecond();
+
+            if (videoPreset.getWidth() != null && videoPreset.getWidth() != width) {
+                width = videoPreset.getWidth();
+            }
+            if (videoPreset.getHeight() != null && videoPreset.getHeight() != height) {
+                height = videoPreset.getHeight();
+            }
+            if (videoPreset.getFramerate() != null && (videoPreset.getFramerate()) != frame_rate) {
+                frame_rate = videoPreset.getFramerate();
+            }
+
+            // TODO implement gamut, target color space
+            var videoOpts = new HashMap<String, String>();
+
+            if (StringUtils.isNullOrEmpty(videoPreset.getVideoBitrate()))
+                videoOpts.put("b", videoPreset.getVideoBitrate());
+
+            target.registerVideoSubstream(videoPreset.getVideoCodecName(), width, height, frame_rate, videoOpts);
+
+            if (!StringUtils.isNullOrEmpty(videoPreset.getVideoPixelFormat()))
+                target.setPixelFormat(FFmpeg.getPixelFormatByName(videoPreset.getVideoPixelFormat()));
+
+            for (AudioSourceSubstream audioSubstream : source.getSubstreams().stream()
+                    .filter(s -> s.getMediaType() == MediaType.AUDIO)
+                    .map(AudioSourceSubstream.class::cast)
+                    .toList()) {
+
+                var inputAudioFormat = audioSubstream.getFormat();
+
+                int samplerate = inputAudioFormat.getSampleRate();
+
+                if (audioPreset.getAudioSampleRate() != null && audioPreset.getAudioSampleRate() != samplerate) {
+                    samplerate = audioPreset.getAudioSampleRate();
+                }
+
+                target.registerAudioSubstream(audioPreset.getAudioCodecName(), samplerate, inputAudioFormat.getChannels(), inputAudioFormat.getChannelLayout(), new HashMap<>());
+            }
+
+            Transcoder transcoder = new Transcoder(source, target, new AudioFilterNone(), new VideoFilterNone(), Double.MAX_VALUE);
+
+            logger.info("Starting transcoder");
+
+            transcoder.transcode();
+            logger.info("Transcoding completed successfully");
+        } catch (Exception e) {
+            logger.error("Transcoder ran into an exception", e);
+            throw e;
+        }
         return null;
     }
 }
+
+
+
+    /*
+            AudioSourceSubstream mediaSourceSubstream = (AudioSourceSubstream) sourceStream.getSubstreams().get(0);
+            AudioFormat audioFormat = mediaSourceSubstream.getFormat();
+
+            targetStream.registerAudioSubstream("libmp3lame", audioFormat, options);
+
+            ZonedDateTime start = ZonedDateTime.now();
+
+            Transcoder transcoder = new Transcoder(sourceStream, targetStream, new AudioFilterNone(), new VideoFilterNone(), 1D);
+
+            Thread transcodingJob = new Thread(() -> {
+                try {
+                    transcoder.transcode();
+                } catch (Exception ex) {
+
+                }
+            });
+
+            double finalDuration = duration;
+            Thread progressDisplayJob = new Thread(() -> {
+                try {
+                    while (true) {
+                        System.out.println(String.format("Progress: %s", Math.max(Math.min(transcoder.getTotalPosition() / finalDuration, 1D), 0D)));
+                        Thread.sleep(100);
+                    }
+                } catch (InterruptedException ex) {
+
+                }
+            });
+
+            transcodingJob.start();
+            progressDisplayJob.start();
+
+            transcodingJob.join();
+            progressDisplayJob.interrupt();
+
+            System.out.println(String.format("Duration: %s", Duration.between(start, ZonedDateTime.now())));
+     */
