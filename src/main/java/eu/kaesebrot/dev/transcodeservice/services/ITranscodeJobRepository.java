25c7e4d068e375ff1c8ffea27154bdadb8f0c9ec
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-09-29 18:25:55 +0200

    [FEATURE] custom retrieval and efficient update methods

--- src/main/java/eu/kaesebrot/dev/transcodeservice/services/ITranscodeJobRepository.java
+++ src/main/java/eu/kaesebrot/dev/transcodeservice/services/ITranscodeJobRepository.java
@@ -4,9 +4,13 @@ import eu.kaesebrot.dev.transcodeservice.constants.ETranscodeServiceStatus;
 import eu.kaesebrot.dev.transcodeservice.models.TranscodeJob;
 import eu.kaesebrot.dev.transcodeservice.models.TranscodePreset;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Stream;
 
 @Repository
@@ -17,4 +21,11 @@ public interface ITranscodeJobRepository extends JpaRepository<TranscodeJob, Lon
     Stream<TranscodeJob> getByPreset(TranscodePreset preset);
     long countByStatus(ETranscodeServiceStatus status);
     void deleteById(Long id);
+    @Modifying
+    @Query("update TranscodeJob u set u.status = :status where u.id = :id")
+    void updateStatus(@Param(value = "id") long id, @Param(value = "status") ETranscodeServiceStatus status);
+    @Query("select t.status from TranscodeJob t where t.id = :id")
+    ETranscodeServiceStatus getStatus(@Param(value = "id") long id);
+    @Query("select t.id, t.status from TranscodeJob t where t in :ids")
+    List<Object[]> findStatuses(@Param(value = "ids") Set<Long> ids);
 }
