fe46ca0cf26eeaae339616bfa8cdb318c186dc15
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-09-29 15:23:23 +0200

    [FEATURE] repository method for finding by job in usage by preset

--- src/main/java/eu/kaesebrot/dev/transcodeservice/services/ITranscodePresetRepository.java
+++ src/main/java/eu/kaesebrot/dev/transcodeservice/services/ITranscodePresetRepository.java
@@ -1,10 +1,13 @@
 package eu.kaesebrot.dev.transcodeservice.services;
 
+import eu.kaesebrot.dev.transcodeservice.models.TranscodeJob;
 import eu.kaesebrot.dev.transcodeservice.models.TranscodePreset;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ITranscodePresetRepository extends JpaRepository<TranscodePreset, Long> {
-
+    Optional<TranscodePreset> findByJobsContains(TranscodeJob job);
 }
