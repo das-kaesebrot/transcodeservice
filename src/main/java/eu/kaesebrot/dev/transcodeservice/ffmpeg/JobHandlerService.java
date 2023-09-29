eeaaa50bae86eaa4cb9c6bd2523023297263ff8b
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-09-29 15:23:41 +0200

    [FEATURE] rename job handler service interface, add submit and progress methods

--- src/main/java/eu/kaesebrot/dev/transcodeservice/ffmpeg/JobHandlerService.java
+++ src/main/java/eu/kaesebrot/dev/transcodeservice/ffmpeg/JobHandlerService.java
@@ -5,8 +5,10 @@ import eu.kaesebrot.dev.transcodeservice.models.TranscodeJob;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-public interface FFmpegJobHandlerService {
+public interface JobHandlerService {
+    void submit(Long jobId);
     void submit(TranscodeJob job);
     List<TranscodeJob> getCompletedTasks() throws NoSuchElementException;
+    double getProgress(Long jobId) throws NoSuchElementException;
     double getProgress(TranscodeJob job) throws NoSuchElementException;
 }
