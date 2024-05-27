06d00a838ec897ea0303ce905bef7b76a1ef091a
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2024-05-28 00:55:39 +0200

    [FEATURE] add GET, PATCH, DELETE and paged GET in preset controller

--- src/main/java/eu/kaesebrot/dev/transcodeservice/api/TranscodePresetRestController.java
+++ src/main/java/eu/kaesebrot/dev/transcodeservice/api/TranscodePresetRestController.java
@@ -1,10 +1,12 @@
 package eu.kaesebrot.dev.transcodeservice.api;
 
 import eu.kaesebrot.dev.transcodeservice.models.TranscodePreset;
-import eu.kaesebrot.dev.transcodeservice.models.rest.SupportedFormats;
 import eu.kaesebrot.dev.transcodeservice.models.rest.TranscodePresetCreation;
+import eu.kaesebrot.dev.transcodeservice.models.rest.TranscodePresetUpdate;
 import eu.kaesebrot.dev.transcodeservice.services.TranscodePresetService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,7 +27,44 @@ public class TranscodePresetRestController {
     )
     @ResponseStatus(HttpStatus.CREATED)
     public TranscodePreset CreateNewPreset(@RequestBody TranscodePresetCreation presetCreation) {
-        // TODO
         return presetService.insertPreset(presetCreation.generateNewPreset());
+    }
+
+    @PatchMapping(
+            value = "presets/{id}",
+            consumes = { "application/json", "application/xml" },
+            produces = { "application/json", "application/xml" }
+    )
+    @ResponseStatus(HttpStatus.OK)
+    public TranscodePreset UpdatePreset(@PathVariable Long id, @RequestBody TranscodePresetUpdate presetUpdate) {
+        return presetService.updatePreset(presetUpdate, id);
+    }
+
+    @GetMapping(
+            value = "presets/{id}",
+            produces = { "application/json", "application/xml" }
+    )
+    @ResponseStatus(HttpStatus.OK)
+    public TranscodePreset GetPreset(@PathVariable Long id) {
+        return presetService.getPreset(id);
+    }
+
+
+    @GetMapping(
+            value = "presets",
+            produces = { "application/json", "application/xml" }
+    )
+    @ResponseStatus(HttpStatus.OK)
+    public Page<TranscodePreset> GetAllPresets(Pageable pageable) {
+        return presetService.getAllPaged(pageable);
+    }
+
+    @DeleteMapping(
+            value = "presets/{id}",
+            produces = { "application/json", "application/xml" }
+    )
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void DeletePreset(@PathVariable Long id) {
+        presetService.deleteById(id);
     }
 }
